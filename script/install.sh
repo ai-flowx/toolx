e89bf09052e7edd018ea451d94bc98ab18b9ac29
Jia <49358172+craftslab@users.noreply.github.com>
2025-01-01 10:54:29 +0800

    refactor(script): refactor code

--- script/install.sh
+++ script/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install python
-sudo apt install python3 python3-dev pkg-config
+sudo apt install -y python3 python3-dev pkg-config
 
 # Install package
 pip3 install -r requirements.txt
